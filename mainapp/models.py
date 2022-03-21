ad6af5d264245ca7dcd02b4696b05d332ae25bcc
SANTI TEERAGUL <santitee@gmail.com>
2022-03-21 23:09:29 +0700

    Completed module

--- mainapp/models.py
+++ mainapp/models.py
@@ -7,6 +7,7 @@ class BlogPost(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
     body = models.TextField()
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
+    tag = models.ManyToManyField('Tag', related_name='posts')
 
 
     def __str__(self):
@@ -15,3 +16,18 @@ class BlogPost(models.Model):
     
     def get_absolute_url(self):
         return reverse('post', args=[str(self.id)])
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    
+    def clean(self):
+        return self.name
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('tag_posts', args=[str(self.name)])
+    
